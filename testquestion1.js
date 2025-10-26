dc206dff67b624d9f7fb911a21330f43620f9984
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-10-26 03:34:28 +0000

    Oct 26, 2025, 3:34 AM

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,23 @@
+let bird
+let background, birdImg;
 
+function preload(){
+    // write code to preload bird.png and background.png
+    // refer to assets folder
+    background = loadImage('assets/background.png');
+    birdImg = loadImage('assets/bird.png');
+}
+
+function setup(){
+    new Canvas(400, 600);
+    // write code to create sprite and load bird image on sprite
+    let bird = new Sprite(200, 300);
+    bird.height = 20;
+    bird.width = 20;
+    bird.img = birdImg;
+    background.img = background;
+}
+
+function draw(){
+    image(background, 0, 0, width, height);
+}
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,23 @@
+let square;
 
+function setup(){
+    new Canvas(400, 600);
+   
+    // code to set the gravity of the world
+    world.gravity.y = 10;
+    // code to create a new square sprite
+    square = new Sprite(200, 300, 30, 30);
+    square.color = "purple";
+    square.collider ="dynamic";
+}
+
+function draw(){
+    background("grey");
+
+    // code to check if mouse is pressed i.e. mouse.presses()
+        // if mouse is pressed, the square sprite "flies" up
+
+    if (mouse.presses()){
+        square.vel.y = -5;
+    }
+}
